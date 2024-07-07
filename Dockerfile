7f32cdd3b7c93f041e0394e6692cb4df7febf707
Alexey-zaliznuak <zaliznuak20@gmail.com>
2024-07-07 21:07:01 +0300

    small updates and docker

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.12-slim
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- src/main.py
+++ src/main.py
@@ -9,23 +9,3 @@ app = FastAPI(docs_url="/api/docs")
 
 
 app.include_router(posts_router)
-
-@app.post("/")
-async def upload_index_page(file: UploadFile = File(...)):
-    file_path = os.path.join(Config.STATIC_DIRECTORY, "index.html")
-
-    os.makedirs(Config.STATIC_DIRECTORY, exist_ok=True)
-
-    with open(file_path, "wb") as buffer:
-        buffer.write(await file.read())
-
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.get("/")
-def get_index_page():
-    path = os.path.join(Config.STATIC_DIRECTORY, "index.html")
-
-    if not os.path.exists(path):
-        raise HTTPException(status_code=404, detail="File not found")
-
-    return FileResponse(path=path, media_type='text/html')
