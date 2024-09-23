a7958109d49f53aa4910a25318b7a88bc74a9904
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-09-23 23:58:45 +0300

    fix

--- src/cases/models.py
+++ src/cases/models.py
@@ -14,8 +14,8 @@ class Case(Base):
     __tablename__ = 'cases'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
-    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
     is_published = Column(Boolean, default=False, nullable=False)
 
