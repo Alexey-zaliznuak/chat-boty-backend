571de17ebadc52374097fa3f0c0e1b00a98d5fa2
Alexey-Zaliznuak <zaliznuak20@gmail.com>
2024-10-27 23:43:09 +0300

    fix types

--- src/share/content/schemas.py
+++ src/share/content/schemas.py
@@ -39,6 +39,32 @@ class BasicBlogContent(BaseModel):
         from_attributes = True
 
 
+class GetBlogContent(BaseModel):
+    id: UUID
+    created_at: datetime
+    updated_at: datetime
+
+    slug: str = Field(max_length=150)
+
+    short_description: str = Field(max_length=300)
+    web_description: Optional[str] = Field(max_length=300)
+    og_description: Optional[str] = Field(max_length=300)
+
+    title: str = Field(max_length=100)
+    web_title: Optional[str] = Field(max_length=100)
+    og_title: Optional[str] = Field(max_length=100)
+
+    content: str = Field()
+    keywords: Optional[str] = Field(max_length=150)
+
+    is_published: bool = Field(False)
+    reading_time: int = Field(gt=0, le=120)
+    views_count: int = Field(gt=0)
+
+    preview_file_id: Optional[UUID] = Field(None)
+    preview_og_file_id: Optional[UUID] = Field(None)
+
+
 class BasicEditableBlogContent(BaseModel):
     short_description: str = Field(max_length=300)
     web_description: str = Field(max_length=300)
